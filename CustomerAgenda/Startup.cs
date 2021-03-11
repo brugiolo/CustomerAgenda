35e1df03c9fb1ea0c1220746f9a6d410a4c76f46
Rafael Brugiolo <brugiolo@hotmail.com>
2021-03-11 08:10:01 -0300

    Incluindo conexão para acesso a dados na Azure

--- CustomerAgenda/Startup.cs
+++ CustomerAgenda/Startup.cs
@@ -23,7 +23,7 @@ namespace CustomerAgenda
         {
             services.AddDbContext<CustomerAgendaContext>(options =>
             {
-                options.UseSqlServer(Configuration.GetConnectionString("CustomerConnection"));
+                options.UseSqlServer(Configuration.GetConnectionString("AzureConnection"));
             });
 
             services.AddAutoMapper(typeof(Startup));
